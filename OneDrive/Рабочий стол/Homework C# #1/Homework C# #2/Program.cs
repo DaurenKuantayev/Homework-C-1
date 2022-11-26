7447bfb6120c1b0e22b814afeb84101ff7a2d7b8
Dauren <daurmegatron@gmail.com>
2022-11-27 01:20:09 +0600

    внес изменения

---
+++ OneDrive/Рабочий стол/Homework C# #1/Homework C# #2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+Console.WriteLine("Напишите первое число"); 
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число"); 
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите третье число");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberB > max)
+{
+    max = numberB;
+}
+else
+{
+max = numberA;
+}
+if (numberC > max)
+{
+    max = numberC;
+    Console.WriteLine($"a = {numberA} b = {numberB} c = {numberC} -> max = {max}");
+}
+else
+{
+   Console.WriteLine($"a = {numberA} b = {numberB} c = {numberC} -> max = {max}");
+}
+
+
---
+++ OneDrive/Рабочий стол/Homework C# #1/Homework C# #4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.WriteLine("Напишите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (i < N-2) 
+{
+Console.WriteLine(i += 2);
+}
