3671baaab6f8d9693f9ac8c10646f3446ffdd4e7
Dauren <daurmegatron@gmail.com>
2022-12-01 20:32:45 +0600

    пробный вариант

---
+++ OneDrive/Рабочий стол/Homework C# №3/Homework C# #1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+void DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    int FirstPartNumb = number / 100 ;
+    int SecondPartNumb = number % 1000;
+    if (FirstPartNumb == SecondPartNumb)
+    {
+        Console.Write($"{number} -> да");
+    }
+    else
+    {
+        Console.Write($"{number} -> нет");
+        Console.Write($"{FirstPartNumb} {SecondPartNumb}");
+    }
+    Console.WriteLine();
+}
+
+DataEntry("Введите пятизначное число: ");
